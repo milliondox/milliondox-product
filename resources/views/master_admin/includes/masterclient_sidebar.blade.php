4a131cd2bea47f67700be93dd12f460c7c3cc830
singhVikrm <vikram.singh@milliondox.com>
2024-11-21 09:59:13 +0530

    ui updates

--- resources/views/master_admin/includes/masterclient_sidebar.blade.php
+++ resources/views/master_admin/includes/masterclient_sidebar.blade.php
@@ -41,7 +41,7 @@
                     <li class="sidebar-list dashboardd">
                         
                     <!--<i class="fa fa-thumb-tack"></i>-->
-                    <a class="sidebar-link sidebar-title link-nav" href="#">
+                    <a class="sidebar-link sidebar-title link-nav" href="{{url('/master_admin/masteradmin')}}">
                         
 <svg   class="d_fade" width="26" height="23" viewBox="0 0 26 23" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_399_168)">
